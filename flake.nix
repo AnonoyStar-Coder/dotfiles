a970de5579a7d7e2c01f340e58b80b967665b8ac
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-06-26 00:19:50 +0530

    p

--- flake.nix
+++ flake.nix
@@ -1,22 +1,24 @@
 {
-    description = "My first flake";
+  description = "Ballu's NixOS config with style and vibes";
 
-    inputs = {
-        nixpkgs = {
-            url = "github:NixOS/nixpkgs/nixos-25.05";
-        };  
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    outputs = { self, nixpkgs, ... }: 
-    let
-        lib = nixpkgs.lib;
-    in
-    {
-        nixosConfigurations = {
-            nixos-btw = lib.nixosSystem {
-                system = "x86_64-linux";
-                modules = [ ./configuration.nix ];
-            };
-        };
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+      ];
+      specialArgs = { inherit home-manager; };
     };
+  };
 }
+
