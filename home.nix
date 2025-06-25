26cdefd1e05ffc445dd4ad53c4b1912e800261f8
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-06-26 00:56:08 +0530

    new

--- home.nix
+++ home.nix
@@ -1,26 +1,53 @@
+# { config, pkgs, ... }:
+
+# {
+#   home.username = "balraj";
+#   home.homeDirectory = "/home/balraj";
+
+#   home.stateVersion = "25.05";
+
+#   programs.home-manager.enable = true;
+#   fonts.fontconfig.enable = true;
+
+#   home.file.".bashrc".source = ./.bashrc;
+  
+#   home.file.".themes".source = ./.themes;
+
+#   home.file.".config/qtile".source = ./qtile;
+
+#   home.file.".config/kitty".source = ./kitty;
+
+#   home.file.".config/alacritty".source = ./alacritty;
+  
+#   home.file.".config/rofi".source = ./rofi;
+  
+#   home.file.".config/spotify-player".source = ./spotify-player;
+
+# } 
+
 { config, pkgs, ... }:
 
 {
   home.username = "balraj";
   home.homeDirectory = "/home/balraj";
-
   home.stateVersion = "25.05";
 
   programs.home-manager.enable = true;
   fonts.fontconfig.enable = true;
 
+  # 🧠 Shell & theming
   home.file.".bashrc".source = ./.bashrc;
-  
+  home.file.".bash-alias".source = ./.bash-alias;
+  home.file.".theme.omp.json".source = ./.theme.omp.json;
   home.file.".themes".source = ./.themes;
 
+  # 🧩 Config directories
   home.file.".config/qtile".source = ./qtile;
-
   home.file.".config/kitty".source = ./kitty;
-
   home.file.".config/alacritty".source = ./alacritty;
-  
   home.file.".config/rofi".source = ./rofi;
-  
   home.file.".config/spotify-player".source = ./spotify-player;
 
-} 
+  # 🎨 Wallpapers (if user also accesses them)
+  home.file.".config/walls".source = ./walls;
+}
