c60d8b770edc364915a5c709bf984c8f5e79c303
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-06-25 23:00:31 +0530

    WIP: Final changes (maybe, idk..)

--- home.nix
+++ home.nix
@@ -9,18 +9,18 @@
   programs.home-manager.enable = true;
   fonts.fontconfig.enable = true;
 
-  home.file.".bashrc".source = /home/balraj/dotfiles/.bashrc;
+  home.file.".bashrc".source = ./.bashrc;
   
-  home.file.".themes".source = /home/balraj/dotfiles/.themes;
+  home.file.".themes".source = ./.themes;
 
-  home.file.".config/qtile".source = /home/balraj/dotfiles/qtile;
+  home.file.".config/qtile".source = ./qtile;
 
-  home.file.".config/kitty".source = /home/balraj/dotfiles/kitty;
+  home.file.".config/kitty".source = ./kitty;
 
-  home.file.".config/alacritty".source = /home/balraj/dotfiles/alacritty;
+  home.file.".config/alacritty".source = ./alacritty;
   
-  home.file.".config/rofi".source = /home/balraj/dotfiles/rofi;
+  home.file.".config/rofi".source = ./rofi;
   
-  home.file.".config/spotify-player".source = /home/balraj/dotfiles/spotify-player;
+  home.file.".config/spotify-player".source = ./spotify-player;
 
 } 
