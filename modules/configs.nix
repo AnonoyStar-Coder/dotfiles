862ed46f37d87edf0bd444598037c2b3514fc13d
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-06-27 19:44:05 +0530

    podfjojdwv

--- modules/configs.nix
+++ modules/configs.nix
@@ -1,7 +1,7 @@
 {
   home.file.".config/qtile".source = ../qtile;
   home.file.".config/kitty".source = ../kitty;
-  #  home.file.".config/nvim".source = ../nvim;
+  home.file.".config/nvim".source = ../nvim;
   home.file.".config/alacritty".source = ../alacritty;
   home.file.".config/rofi".source = ../rofi;
   home.file.".config/spotify-player".source = ../spotify-player;
