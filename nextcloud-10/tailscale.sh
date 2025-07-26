dbfb3622871131b3e49690ee979345f7e7fa12e2
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-07-26 16:00:04 +0000

    ADD: new files from ubuntu-server

---
+++ nextcloud-10/tailscale.sh
@@ -0,0 +1,7 @@
+ sudo tailscale funnel --bg --set-path / http://localhost:8080
+ sudo tailscale funnel --bg --set-path /ntfy http://localhost:8081
+ sudo tailscale funnel --bg --set-path /jellyfin http://localhost:8096
+ sudo tailscale funnel --bg --set-path /terminal http://localhost:8082
+ sudo tailscale funnel --bg --set-path /docker http://localhost:8083
+ sudo tailscale funnel --bg --set-path /code http://localhost:8443
+ sudo tailscale funnel --bg --set-path /minecraft http://localhost:25565
